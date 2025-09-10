7a79fde5335bf9295cff4c62066c6f76d983beba
Najiluc <juli.navarro.13@gmail.com>
2025-09-09 21:03:54 -0500

    add response entity

--- src/main/java/com/nequi/franchise/franchise/controllers/FranchiseController.java
+++ src/main/java/com/nequi/franchise/franchise/controllers/FranchiseController.java
@@ -5,7 +5,9 @@ import com.nequi.franchise.franchise.responses.utils.BasicIdNameResponse;
 import com.nequi.franchise.franchise.services.FranchiseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,7 @@ public class FranchiseController {
     private final FranchiseService franchiseService;
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public BasicIdNameResponse createFranchise(@Valid @RequestBody FranchiseRequest franchiseRequest){
-        return franchiseService.createFranchise(franchiseRequest);
+    public ResponseEntity<BasicIdNameResponse> createFranchise(@Valid @RequestBody FranchiseRequest franchiseRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(franchiseService.createFranchise(franchiseRequest));
     }
 }
